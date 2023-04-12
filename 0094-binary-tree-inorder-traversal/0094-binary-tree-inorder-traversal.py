8c01d34f48e29c290f8477ce566fb28e0cb4406b
Ephrem-shimels21 <106282573+Ephrem-shimels21@users.noreply.github.com>
2023-04-13 01:37:31 +0300

    Time: 29 ms (78.35%), Space: 13.8 MB (93.71%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -10,11 +10,32 @@ class Solution:
     def __init__(self):
         self.result = []
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # if root == None:
+        #     return
+        # self.inorderTraversal(root.left)
+        # self.result.append(root.val)
+        # self.inorderTraversal(root.right)
+        # return self.result
         if root == None:
-            return
-        self.inorderTraversal(root.left)
-        self.result.append(root.val)
-        self.inorderTraversal(root.right)
+            return self.result
+        stack = [root]
+        visited = []
+        
+        # print(stack[-1].right)
+        while stack:
+            # break
+            check = stack[-1]
+            if check.left and check.left not in visited:
+                stack.append(check.left)
+            else:
+                current = stack.pop()
+                visited.append(current)
+                self.result.append(current.val)
+                if current.right:
+                    stack.append(current.right)
         return self.result
+                
+                
+        
         
         
