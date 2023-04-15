4b4d56eac0edb831f314d4f8a9a6f30cf7b5dc2d
Ephrem-shimels21 <106282573+Ephrem-shimels21@users.noreply.github.com>
2023-04-15 13:41:22 +0300

    Time: 68 ms (52.65%), Space: 15.9 MB (5.89%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        store = {}
+        for i,x in enumerate(nums):
+            store[i] = (x,i)
+           
+        original = nums[::]
+        nums.sort()
+        i = 0
+        j = len(nums) - 1       
+        while True:
+            if nums[i] + nums[j] > target:
+                j -= 1
+            elif nums[i] + nums[j] < target:
+                i += 1
+            elif nums[i] + nums[j] == target:
+                ans = []
+                for ele in store.values():
+                    if nums[i] == ele[0] or nums[j] == ele[0]:
+                        ans.append(ele[1])
+                
+                return ans
+        
