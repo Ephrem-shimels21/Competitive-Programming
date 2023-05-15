09482402ed9f4881876b3d6ef48e2cb7fcba8eda
Ephrem-shimels21 <106282573+Ephrem-shimels21@users.noreply.github.com>
2023-05-15 23:46:01 +0300

    Time: 113 ms (5.15%), Space: 19 MB (11.00%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = list(s)
+        print(s)
+        new = ''
+        for i in s:
+            print(ord(i))
+            if (ord(i) >= 65 and ord(i) < 91) or (ord(i) >= 97 and ord(i) < 123) or (ord(i) <= 57 and ord(i) >= 48):
+                new += i
+        new = new.lower()
+        reversed_s = new[::-1]
+        if new == reversed_s:
+            return True
+        return False
+        
