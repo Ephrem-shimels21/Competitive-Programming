45a4f18a6e2db60b78220032fbc695ef96d7e427
Ephrem-shimels21 <eph21sz@gmail.com>
2023-07-06 10:48:02 +0300

    modify the palindrome linked List

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -6,16 +6,38 @@
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         list: List = []
- 
-        if not head:
+        current = head
+        
+        while current:
+            list.append(str(current.val))
+            current = current.next
+        
+        
+        forward = "".join(list)
+        backward = "".join(list[::-1])
+        
+        if forward == backward:
             return True
-        node = head
-        while node != None:
-            list.append(node.val)
-            node = node.next
+        return False
  
-        if list == list[::-1]:
-            return True
-        else:
-            return False
+
+
+
+
+
+
+
+
+
+#         if not head:
+#             return True
+#         node = head
+#         while node != None:
+#             list.append(node.val)
+#             node = node.next
+ 
+#         if list == list[::-1]:
+#             return True
+#         else:
+#             return False
         
