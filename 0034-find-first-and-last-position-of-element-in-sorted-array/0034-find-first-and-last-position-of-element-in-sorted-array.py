d5d2195e3856a07f102fd40d053c1cfcfb39de0e
Ephrem-shimels21 <106282573+Ephrem-shimels21@users.noreply.github.com>
2023-10-09 14:19:09 +0300

    Update 0034-find-first-and-last-position-of-element-in-sorted-array.py

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -10,19 +10,14 @@ class Solution(object):
         ending = len(nums) - 1
         
         while initial <= ending:
-            # print(initial, ending,nums[ending])
             if nums[initial] == target and nums[ending] == target:
-                # print("A")
                 return [initial, ending]
             elif nums[initial] != target and nums[ending] != target:
-                # print("B")
                 initial += 1
                 ending -= 1
             elif nums[initial] != target:
-                # print("C")
                 initial += 1
             elif nums[ending] != target:
-                # print("D")
                 ending -= 1
         return [-1,-1]
         
