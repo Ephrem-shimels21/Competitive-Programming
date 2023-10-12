e5ce44117cc84809acbfcb28f6f291f145537bac
Ephrem-shimels21 <106282573+Ephrem-shimels21@users.noreply.github.com>
2023-10-12 10:32:53 +0300

    Time: 461 ms (57.87%), Space: 25.7 MB (31.63%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -4,6 +4,20 @@ class Solution(object):
         :type arr: List[int]
         :rtype: int
         """
-        for index,num in enumerate(arr):
-            if num > arr[index + 1]:
-                return index
+        # for index,num in enumerate(arr):
+        #     if num > arr[index + 1]:
+        #         return index
+        left = 0
+        right = len(arr) - 1
+        mid = (left + right) // 2
+        
+        while left < right:
+            if arr[mid] < arr[mid + 1]:
+                left = mid + 1
+                mid = (left + right) // 2
+            elif arr[mid] > arr[mid + 1]:
+                right = mid
+                mid = (left + right) // 2
+            # if left == right:
+            #     return right
+        return mid
