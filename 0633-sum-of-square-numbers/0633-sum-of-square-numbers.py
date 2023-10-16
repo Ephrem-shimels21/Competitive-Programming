f206688a0b03c48f28e404d2115cbb27d2e8c9c7
Ephrem-shimels21 <106282573+Ephrem-shimels21@users.noreply.github.com>
2023-10-16 16:02:23 +0300

    Time: 80 ms (60.25%), Space: 13.2 MB (81.37%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -4,20 +4,14 @@ class Solution(object):
         :type c: int
         :rtype: bool
         """
-        possiblities = []
-        if c > 4:
-            possibilites = [x for x in range(int(c ** 0.5) + 1)]
-        else:
-            possibilites = [x for x in range(c + 1)]
-        
         left  = 0
-        right = len(possibilites) - 1
+        right = int(c ** 0.5)
         while left <= right:
-            if possibilites[left] ** 2 + possibilites[right] ** 2 == c:
+            if left ** 2 + right ** 2 == c:
                 return True
-            elif possibilites[left] ** 2 + possibilites[right] ** 2  < c:
-                left += 1
-            elif possibilites[left] ** 2 + possibilites[right] ** 2 > c:
+            elif left ** 2 + right ** 2 > c:
                 right -= 1
+            elif left ** 2 + right ** 2 < c:
+                left += 1
         return False
         
