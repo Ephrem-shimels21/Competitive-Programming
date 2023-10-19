2fb5c067ba437925be749b18691094278f91a431
Ephrem-shimels21 <106282573+Ephrem-shimels21@users.noreply.github.com>
2023-10-19 15:16:42 +0300

    Time: 19 ms (48.38%), Space: 13.5 MB (29.56%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -5,22 +5,35 @@ class Solution(object):
         :rtype: str
         """
         stack = []
-        ans = ""
-        pattern = r"[ /]"
-        result = re.split(pattern,path)
+        cur = ""
+        for c in path + "/":
+            if c == "/":
+                if cur == "..":
+                    if stack: stack.pop()
+                elif cur != "" and cur != ".":
+                    stack.append(cur)
+                cur = ""
+            else:
+                cur += c
+        return "/" + "/".join(stack)
+        
+#         stack = []
+#         ans = ""
+#         pattern = r"[/]"
+#         result = re.split(pattern,path)
         
        
-        for char in result:
-            if stack and  char == ".." and stack[-1] != "/" :
-                stack.pop()
-            elif len(char) != 0 and char != ".." and char != ".":
-                stack.append(char)
+#         for char in result:
+#             if stack and  char == ".." and stack[-1] != "/" :
+#                 stack.pop()
+#             elif len(char) != 0 and char != ".." and char != ".":
+#                 stack.append(char)
         
-        if not stack:
-            return "/"
-        while stack:
-            current = stack.pop()
-            ans = "/" + current + ans
+#         if not stack:
+#             return "/"
+#         while stack:
+#             current = stack.pop()
+#             ans = "/" + current + ans
             
-        return  ans
+#         return  ans
         
