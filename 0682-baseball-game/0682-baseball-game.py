d1e0740587716e48c01f0a191d413497b5a0610c
Ephrem Shimels <106282573+Ephrem-shimels21@users.noreply.github.com>
2023-11-21 14:11:51 +0300

    Time: 45 ms (76.79%), Space: 16.6 MB (14.26%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -1,15 +1,20 @@
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
         stack = []
+        total = 0
         operands = "CD+"
         for op in operations:
             if op not in operands:
                 stack.append(int(op))
+                total += int(op)
             elif op == "C":
-                stack.pop()
+                a = stack.pop()
+                total -= a
             elif op == "D":
                 last = stack[-1]
-                stack.append(last * 2)
+                double = last * 2
+                stack.append(double)
+                total += double
             elif op  == "+":
                 last = stack.pop()
                 first = stack.pop()
@@ -17,5 +22,6 @@ class Solution:
                 stack.append(first)
                 stack.append(last)
                 stack.append(summ)
-        return sum(stack)
+                total += summ
+        return total
             
