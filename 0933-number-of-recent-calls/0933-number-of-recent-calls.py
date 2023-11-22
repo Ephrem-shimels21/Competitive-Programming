86621f8f18eb44e273494beab792d73d18ae0934
Ephrem Shimels <106282573+Ephrem-shimels21@users.noreply.github.com>
2023-11-22 14:58:33 +0300

    Time: 236 ms (30.54%), Space: 21.3 MB (97.42%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.py
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -2,18 +2,15 @@ class RecentCounter:
 
     def __init__(self):
         self.qeue = deque()
-        self.count = 0
         
 
     def ping(self, t: int) -> int:
         self.qeue.append(t)
-        self.count += 1
         
         while self.qeue[0] < t - 3000:
             self.qeue.popleft()
-            self.count -= 1
         
-        return self.count
+        return len(self.qeue)
             
         
 
