a85aafa7908e71afbdbb07dd96ee12376b532d8c
Ephrem Shimels <106282573+Ephrem-shimels21@users.noreply.github.com>
2024-01-02 21:13:24 +0300

    Added solution for Move Zeroes

--- week1/move-zeroes.py
+++ week1/move-zeroes.py
@@ -3,9 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums))[::-1]: 
-                if nums[i] == 0: 
-                    nums.pop(i) 
-                    nums.append(0)
-        return nums
+        holder = 0
+        seeker = 0
+        while seeker < len(nums):
+            if nums[seeker] != 0:
+                nums[holder], nums[seeker] =  nums[seeker], nums[holder]
+                holder += 1
+            seeker += 1
+
         
