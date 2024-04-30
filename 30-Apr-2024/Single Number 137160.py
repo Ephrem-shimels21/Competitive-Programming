c3461eab747c2254caa73d01ccb5ad69d282b5ec
Ephrem Shimels <106282573+Ephrem-shimels21@users.noreply.github.com>
2024-04-30 19:12:40 +0300

    Created 'Single Number' by A2SV Hub

---
+++ 30-Apr-2024/Single Number 137160.py
@@ -0,0 +1,10 @@
+# Problem: Single Number - https://leetcode.com/problems/single-number/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = nums[0]
+
+        for i in range(1, len(nums)):
+            res = res ^ nums[i]
+        
+        return res
