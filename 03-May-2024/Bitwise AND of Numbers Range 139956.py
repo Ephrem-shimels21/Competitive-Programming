62fab596bbdd2b6d2b067e9c3ae672c0fce81910
Ephrem Shimels <106282573+Ephrem-shimels21@users.noreply.github.com>
2024-05-03 22:39:48 +0300

    Created 'Bitwise AND of Numbers Range' by A2SV Hub

---
+++ 03-May-2024/Bitwise AND of Numbers Range 139956.py
@@ -0,0 +1,9 @@
+# Problem: Bitwise AND of Numbers Range - https://leetcode.com/problems/bitwise-and-of-numbers-range/
+
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        while left < right:
+            right &= right - 1
+        
+        return right
+        
