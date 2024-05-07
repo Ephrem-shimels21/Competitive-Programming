ef74846ce4aed5d6608795b8d6e8f0d115173a4c
Ephrem Shimels <106282573+Ephrem-shimels21@users.noreply.github.com>
2024-05-07 19:14:06 +0300

    Created 'C - ANDy Session' by A2SV Hub

---
+++ A2SV G5 - Contest #15 07-May-2024/C - ANDy Session 142623.py
@@ -0,0 +1,26 @@
+# Problem: C - ANDy Session - https://codeforces.com/gym/522079/problem/C
+
+tests = int(input())
+
+
+def find_max_result(arr, k):
+    length = len(arr)
+    result = 0
+
+    for pos in range(30, -1, -1):
+        count = sum(1 for num in arr if num & (1 << pos))
+        operations_needed = length - count
+
+        if k >= operations_needed:
+            result |= 1 << pos
+            k -= operations_needed
+
+    return result
+
+
+for _ in range(tests):
+    n, k = map(int, input().split())
+    arr = list(map(int, input().split()))
+    arr.sort()
+
+    print(find_max_result(arr, k))
