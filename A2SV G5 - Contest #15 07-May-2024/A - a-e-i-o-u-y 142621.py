a4e4368959067180569143ed2bcd131ecbcd2870
Ephrem Shimels <106282573+Ephrem-shimels21@users.noreply.github.com>
2024-05-07 19:13:13 +0300

    Created 'A - a-e-i-o-u-y' by A2SV Hub

---
+++ A2SV G5 - Contest #15 07-May-2024/A - a-e-i-o-u-y 142621.py
@@ -0,0 +1,17 @@
+# Problem: A - a-e-i-o-u-y - https://codeforces.com/gym/522079/problem/A
+
+length = int(input())
+word = input()
+
+vowel = "aeiouy"
+
+stack = []
+
+for letter in word:
+    if letter in vowel and stack and stack[-1] in vowel:
+        continue
+    else:
+        stack.append(letter)
+
+
+print("".join(stack))
