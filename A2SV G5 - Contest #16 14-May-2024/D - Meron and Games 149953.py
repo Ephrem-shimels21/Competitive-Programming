a16369dedef63366ce5d42ec0dabf0f1c29c862b
Ephrem Shimels <106282573+Ephrem-shimels21@users.noreply.github.com>
2024-05-14 18:41:28 +0300

    Created 'D - Meron and Games' by A2SV Hub

---
+++ A2SV G5 - Contest #16 14-May-2024/D - Meron and Games 149953.py
@@ -0,0 +1,43 @@
+# Problem: D - Meron and Games - https://codeforces.com/gym/523525/problem/D
+
+from collections import Counter
+import sys, threading
+
+
+def main():
+    n = int(input())
+    arr = list(map(int, input().split()))
+
+    arr_count = Counter(arr)
+    unique = list(set(arr))
+    unique.sort()
+
+    def dp(idx):
+        if idx >= len(unique):
+            return 0
+
+        if idx not in memo:
+            take = 0
+            if idx < len(unique) - 1:
+                if unique[idx + 1] - 1 == unique[idx]:
+                    take = arr_count[unique[idx]] * unique[idx] + dp(idx + 2)
+                else:
+                    take = arr_count[unique[idx]] * unique[idx] + dp(idx + 1)
+            else:
+                take = arr_count[unique[idx]] * unique[idx]
+
+            leave = dp(idx + 1)
+            memo[idx] = max(take, leave)
+
+        return memo[idx]
+
+    memo = {}
+    print(dp(0))
+
+
+sys.setrecursionlimit(1 << 30)
+threading.stack_size(1 << 27)
+
+main_thread = threading.Thread(target=main)
+main_thread.start()
+main_thread.join()
