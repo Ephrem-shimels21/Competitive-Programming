d2017484ae129e9b086b45071baa555f832e6af4
Ephrem Shimels <106282573+Ephrem-shimels21@users.noreply.github.com>
2024-06-14 23:14:45 +0300

    Created 'Escape The Ghosts' by A2SV Hub

---
+++ 14-Jun-2024/Escape The Ghosts 168490.py
@@ -0,0 +1,12 @@
+# Problem: Escape The Ghosts - https://leetcode.com/problems/escape-the-ghosts/
+
+class Solution:
+    def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
+        tx, ty = target[0], target[1]
+        my_distance = max(tx, 0) - min(tx, 0) + max(ty, 0) - min(ty, 0)
+        for ghost in ghosts:
+            gx, gy = ghost[0], ghost[1]
+            ghost_distance = max(gx,tx) - min(gx,tx) + max(gy, ty) - min(gy, ty)
+            if ghost_distance <= my_distance:
+                return False
+        return True 
