f8a1702d582076bbfc4214ff1fa66fb8ada8b697
Ephrem Shimels <106282573+Ephrem-shimels21@users.noreply.github.com>
2024-06-15 23:40:02 +0300

    Created 'Binary Tree Inorder Traversal' by A2SV Hub

---
+++ 15-Jun-2024/Binary Tree Inorder Traversal 168758.py
@@ -0,0 +1,20 @@
+# Problem: Binary Tree Inorder Traversal - https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.ans = []
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+
+        if root:
+            self.inorderTraversal(root.left)
+            self.ans.append(root.val)
+            self.inorderTraversal(root.right)
+        
+        return self.ans
+        
