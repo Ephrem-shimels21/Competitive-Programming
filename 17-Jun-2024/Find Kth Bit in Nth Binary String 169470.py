eba0cca1e77ebd0eb25fee4f846f616279f4693b
Ephrem Shimels <106282573+Ephrem-shimels21@users.noreply.github.com>
2024-06-18 01:03:12 +0300

    Created 'Find Kth Bit in Nth Binary String' by A2SV Hub

---
+++ 17-Jun-2024/Find Kth Bit in Nth Binary String 169470.py
@@ -0,0 +1,18 @@
+# Problem: Find Kth Bit in Nth Binary String - https://leetcode.com/problems/find-kth-bit-in-nth-binary-string/
+
+class Solution:
+    def FindKthBit(self,n):
+        if n == 1:
+            return  "0"
+        elif n != 1:
+            outpuTemp=""
+            for bits in self.FindKthBit(n-1):
+                if bits == "1":
+                    outpuTemp += "0"
+                else:
+                    bits= "1"
+                    outpuTemp += bits
+        return self.FindKthBit(n-1) + "1" + outpuTemp[::-1]
+    
+    def findKthBit(self, n: int, k: int) -> str:
+        return self.FindKthBit(n)[k-1]
